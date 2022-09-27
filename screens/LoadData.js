52913d579ef1fa8837e60dee568201e0326f5923
yasindu1997 <yasindusathsara@gmail.com>
2022-09-27 11:30:35 +0530

    flat list done

--- screens/LoadData.js
+++ screens/LoadData.js
@@ -1,4 +1,4 @@
-import { View, Text, FlatList } from 'react-native'
+import { View, Text, FlatList,TouchableOpacity } from 'react-native'
 import React, { useEffect, useState } from 'react'
 
 export default function LoadData() {
@@ -6,32 +6,22 @@ export default function LoadData() {
     const [posts, setPosts] = useState([]);
 
     useEffect(() => {
-
         fetch('https://jsonplaceholder.typicode.com/posts')
             .then((response) => response.json())
-            .then((json) => JSON.stringify(json))
-            .then((o) => {
-
-                const new_posts = [];
-
-                const new_Arr = JSON.parse(o);
-                for(const post of new_Arr){
-                    new_posts.push(post);
-                }
-            })
-
+            .then((json) => setPosts(json));
     })
 
     return (
-        <View>
-            {/* <FlatList
-        data={posts}
-        renderItem={(post)=>(
-            <Text>{post}</Text>
-        )}
-        keyExtractor={post => post.id}
-      /> */}
-      <Text>Hello</Text>
+        <View style={{padding:20}}>
+            <FlatList
+                data={posts}
+                renderItem={({ item }) =>
+                    <TouchableOpacity style={{borderWidth:1, marginBottom:'5%', padding:5}} onPress={()=>{console.log("hello");}}>
+                        <Text style={{marginBottom:10,fontWeight:'bold'}} >{item.title}</Text>
+                        <Text style={{marginBottom:10}} >{item.body}</Text>
+                    </TouchableOpacity>
+                }
+            />
         </View>
     )
 }
